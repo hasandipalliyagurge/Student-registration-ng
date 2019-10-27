06b4f16f71d2c25857f3bfd5aa912d13344a3718
hasandi_palliaguruge <pghasandi@gmail.com>
2019-10-27 12:12:09 +0530

    new class

--- src/app/sign-up/sign-up.component.ts
+++ src/app/sign-up/sign-up.component.ts
@@ -4,13 +4,19 @@ import { NgForm } from '@angular/forms';
 import { AngularFirestore } from '@angular/fire/firestore';
 import { Router,ActivatedRoute }from '@angular/router';
 
+ class  Data{
+  UserName:string;
+  PassWord:string;
+  Email:string;
+ }
+
 @Component({
   selector: 'app-sign-up',
   templateUrl: './sign-up.component.html',
   styleUrls: ['./sign-up.component.css']
 })
 export class SignUpComponent implements OnInit{
-
+data : Data;
 user: User;
 emailPattern="^[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,4}$";
 
